3e9d6ffde0b0198f1e54fae36b89b37060485ced
yuliyavitkovsky <yuliya.vitkovsky@gmail.com>
2023-11-09 02:44:24 +0200

    cw

---
+++ src/main/java/config/AppiumConfig.java
@@ -0,0 +1,43 @@
+package config;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumConfig {
+
+public static AppiumDriver<MobileElement> driver;
+
+//        "platformName": "Android",
+//            "deviceName": "Nex5",
+//            "platformVersion": "8.0",
+//            "appPackage": "com.sheygam.contactapp",
+//            "appActivity": ".SplashActivity"
+
+    @BeforeSuite
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Nex5");
+      //  capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0");
+        capabilities.setCapability("appPackage", "com.sheygam.contactapp");
+        capabilities.setCapability("appActivity", ".SplashActivity");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:/Tools/contacts-android.apk");
+
+        driver = new AppiumDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), capabilities);
+    }
+
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/tests/LaunchTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import config.AppiumConfig;
+import org.testng.annotations.Test;
+
+public class LaunchTest extends AppiumConfig {
+
+    @Test
+    public void launch(){
+
+    }
+
+}
